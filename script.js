c3e56c6eed26832ece1f741b0e39e1a44a4dd3b8
Yuvraj <83761474+Yuvraj-777@users.noreply.github.com>
2023-12-10 20:03:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+/* darkmode */
+function addDarkmodeWidget() {
+  new Darkmode().showWidget();
+}
+window.addEventListener('load', addDarkmodeWidget);
+const options = {
+    bottom: '64px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  
+  darkmode.showWidget();
+
+  /*loader */
+   window.addEventListener("load", () => {
+     const loader = document.querySelector(".loader");
+  
+     loader.classList.add("loader--hidden");
+  
+     loader.addEventListener("transitionend", () => {
+       document.body.removeChild(loader);
+     });
+   });
